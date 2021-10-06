9c3cd4b0fb5c8188205b42977dd93afd52b4b4b9
Marshneil13 <reachmarshneil@hotmail.com>
2021-10-06 14:46:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const container = document.querySelector('.container');
+const baseURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+
+for(let i=1; i<=151; i++){
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon')
+    const pokeRoll = document.createElement('span');
+    pokeRoll.innerText = `#${i}`;
+    const newImg = document.createElement('img');
+    newImg.src =`${baseURL}${i}.png`
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(pokeRoll);
+    container.appendChild(pokemon);
+}
